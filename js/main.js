8e08e9e6741b922cf8197ad1f79f7468e085670d
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-28 15:31:37 +0100

    feat: milestone1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            toDos: [
+                {
+                    task: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    task: 'lavare la macchina',
+                    done: false
+                },
+                {
+                    task: 'pagare bolletta',
+                    done: true
+                },
+                {
+                    task: 'chiamare la zia',
+                    done: true
+                },
+                {
+                    task: 'andare dal medico',
+                    done: false
+                }
+            ]
+        }
+    }
+}).mount('#app');
