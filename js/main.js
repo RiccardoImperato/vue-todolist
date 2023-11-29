af4a1553fc759fa5edf61d66cbc08aa3d46e0e9e
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-29 10:16:58 +0100

    feat: bonus1/2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// Descrizione:
-// Rifare l'esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
-// Bonus:
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
-
 const { createApp } = Vue;
 
 const toDos = [
@@ -42,8 +26,6 @@ const toDos = [
 ];
 
 createApp({
-
-
     data() {
         return {
             toDos,
@@ -61,6 +43,9 @@ createApp({
                 this.toDos.push({ task: this.newTask, done: false });
                 console.log(this.newTask);
             };
+        },
+        doneUndone(index) {
+            this.toDos[index].done = !this.toDos[index].done;
         }
     }
 }).mount('#app');
